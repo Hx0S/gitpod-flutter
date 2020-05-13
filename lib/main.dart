96d68a7b623f3cd753a19f71d499d135a4e46bc0
Hx0S <nikbragvipmen@gmail.com>
2020-05-13 12:34:15 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:gitpodflutter/search_list.dart';
 
-import 'SearchListViewExample.dart';
-// import 'ViewPdf.dart';
-
-// void main() {
-//   runApp(
-//   );
-// }
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    title: 'ENTdbCenter',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('ENTdbCenter'),
+    return new MaterialApp(
+      title: 'GainENT',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        brightness: Brightness.light,
+        primarySwatch:Colors.yellow,
+        primaryColor: Color(0xFFFFBB54),
+        accentColor: Color(0xFFECEFF1),
       ),
-      body: SearchListViewExample(),
-
-    ),
-  );
+      home: new SearchList(),
+    );
   }
 }
+// import 'package:flutter/material.dart';
 
+// import 'SearchListViewExample.dart';
+// // import 'ViewPdf.dart';
+
+// // void main() {
+// //   runApp(
+// //   );
+// // }
+// void main() => runApp(MyApp());
 
 // class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       title: 'ENTdbCenter',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
+//     title: 'ENTdbCenter',
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: Text('ENTdbCenter'),
 //       ),
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text("ЕНТdbСenter"),
-//         ),
-//         body: SearchListViewExample(),
-//       ),
-//     );
+//       body: SearchListViewExample(),
+
+//     ),
+//   );
 //   }
 // }
+
+
+// // class MyApp extends StatelessWidget {
+// //   // This widget is the root of your application.
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'ENTdbCenter',
+// //       theme: ThemeData(
+// //         primarySwatch: Colors.blue,
+// //       ),
+// //       debugShowCheckedModeBanner: false,
+// //       home: Scaffold(
+// //         appBar: AppBar(
+// //           title: Text("ЕНТdbСenter"),
+// //         ),
+// //         body: SearchListViewExample(),
+// //       ),
+// //     );
+// //   }
+// // }
